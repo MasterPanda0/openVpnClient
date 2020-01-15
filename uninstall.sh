442ac695fe4c9294d2fe9816a6561f490f704643
Hubertus Hans <56813171+MasterPanda0@users.noreply.github.com>
2020-01-15 20:21:12 +0700

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Activity="`pwd`/vpnAuto.sh > `pwd`/vpnAuto.log 2>&1"
+Job="@reboot sleep 60 && $Activity"
+
+(crontab -l | grep -v -F "$Activity") | crontab -
+ehco "Finished! please restart your system"
