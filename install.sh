774126bf920303aff1261bfc204f432684216fc4
Hubertus Hans <56813171+MasterPanda0@users.noreply.github.com>
2020-01-15 20:22:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 Activity="`pwd`/vpnAuto.sh > `pwd`/vpnAuto.log 2>&1"
 Job="@reboot sleep 60 && $Activity"
 
+sudo chmod +x vpnAuto.sh
+sudo chmod +x uninstall.sh
+
 (crontab -l | grep -v -F "$Activity"; echo "$Job") | crontab -
 ehco "Instalation finished! please restart your system"
